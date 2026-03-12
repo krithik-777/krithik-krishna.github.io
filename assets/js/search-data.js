a032b798e0d8f908d5d9671c1f0eb3e07e0f25d2
krithik-777 <krithik1292@gmail.com>
2026-03-12 07:12:01 +0000

    Deploying to gh-pages from @ krithik-777/krithik-krishna.github.io@5d91b524ee75edca3af2211030527935160f7707 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-excited-to-share-that-i-ve-joined-whirlybird-electronics-pvt-ltd-as-an-embedded-software-engineer-in-vasai",
+          title: 'Excited to share that I’ve joined Whirlybird Electronics Pvt. Ltd. as an Embedded...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
